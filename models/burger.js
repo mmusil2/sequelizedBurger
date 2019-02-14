19b373527140749fba2fb87938ed7d537317106d
mmusil2 <mmusil2@kent.edu>
2019-02-14 16:59:45 -0500

    more sequelized setup

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,34 @@
-var orm = require("../config/orm");
+// var orm = require("../config/orm");
 
-var burger = {
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
-            cb(res);
-        });
-    },
-    insertOne: function(burgerName, cb) {
-        orm.insertOne("burgers", burgerName, function(res) {
-            cb(res);
-        });
-    },
-    updateOne: function(condition, cb) {
-        orm.update("burgers", condition, function(res) {
-          cb(res);
-        });
-    }
+// var burger = {
+//     selectAll: function(cb) {
+//         orm.selectAll("burgers", function(res) {
+//             cb(res);
+//         });
+//     },
+//     insertOne: function(burgerName, cb) {
+//         orm.insertOne("burgers", burgerName, function(res) {
+//             cb(res);
+//         });
+//     },
+//     updateOne: function(condition, cb) {
+//         orm.update("burgers", condition, function(res) {
+//           cb(res);
+//         });
+//     }
+// };
+
+
+module.exports = function(sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    return Burger;
 };
-
-
-module.exports = burger;
